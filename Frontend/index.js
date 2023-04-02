75193c1cc53527f81eb0343c94693adf098ed49e
SAYALI JADHAV <jadhavsayalip2316@gmail.com>
2023-04-02 22:34:36 +0530

    done

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,3 +1,9 @@
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
+
+const plansEl=document.getElementById("see-all-plans")
+
+plansEl.addEventListener("click",()=>{
+    window.location.href="productpage.html"
+})
